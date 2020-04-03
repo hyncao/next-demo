8edee46dc78d463ebfc016445cfdfc60e7e99dfe
菠菜 <277148066@qq.com>
2020-04-03 11:07:34 +0800

    引入antd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
-import 'styles/index.scss'
+import 'antd/dist/antd.css';
+import 'styles/index.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
